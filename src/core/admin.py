30df568eadc0b748fee696f5f3210be4945829a6
Thiago Garcia <thiagogds14@gmail.com>
2012-11-16 14:37:07 -0200

    Add models para Absenteismo e entradas do Absenteismo.

---
+++ src/core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from models import Absenteeism, AbsenteeismEntry
+
+admin.site.register(AbsenteeismEntry)
+admin.site.register(Absenteeism)
--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Absenteeism(models.Model):
+    medical_obs = models.TextField(blank=True, null=True)
+    adm_obs = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class AbsenteeismEntry(models.Model):
+    atested_begin = models.DateField(null=False, blank=False)
+    atested_end = models.DateField(null=False, blank=False)
+    revised_begin = models.DateField(null=False, blank=False)
+    revised_end = models.DateField(null=False, blank=False)
+    absenteeism = models.ForeignKey(Absenteeism, null=False, blank=False)
